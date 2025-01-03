af4f3a0cb05a9afd3e694ce598ae3dd0041d2e36
Erwin Zieger <e-zieger@hotmail.com>
2025-01-03 17:09:27 -0300

    Update tienda_online.py

--- tienda_online.py
+++ tienda_online.py
@@ -5,16 +5,16 @@ total = descuento1 - dinero
 if dinero > 1000 and miembro == 'si' :
     print()
     print('*** Felicidades eres miembro de la tienda y tienes un 10% de descuento! ***')
-    print(f'Monto total de la compra: {dinero}')
-    print(f'Monto total con descuento premium: {descuento1}')
+    print(f'Monto total de la compra: ${dinero}')
+    print(f'Monto total con descuento premium: ${descuento1}')
 elif miembro == 'si' and  dinero <= 1000 :
     print()
     print('Por solo ser miembro tendras un 5% de Descuento!')
-    print(f'Tu monto es de {dinero * 0.95}')
+    print(f'Tu monto es de ${dinero * 0.95}')
 elif miembro == 'no' and dinero > 1000:
     print()
-    print(f'No sos miembro, pero superaste los $1000. Tu monto total es de {dinero * 0.97} con un 3% OFF.')
+    print(f'No sos miembro, pero superaste los $1000. Tu monto total es de ${dinero * 0.97} con un 3% OFF.')
     print('Te invitamos a que seas socio de nuestra marca!')
 else:
     print()
-    print(f'No sos miembro, ni tampoco superaste los $1000. Tu monto total es de {dinero}')
+    print(f'No sos miembro, ni tampoco superaste los $1000. Tu monto total es de ${dinero}')
